b2d6378702fc2a38c94fba72aa2fb01c99f98922
anwark7 <anwarkhairuddin11@gmail.com>
2022-08-01 12:34:40 +0700

    feat: add faskes marker

---
+++ src/components/icon/faskesIcon.js
@@ -0,0 +1,10 @@
+import L from "leaflet";
+
+const faskesIcon = new L.Icon({
+    iconUrl: "/assets/icons/faskes.png",
+    iconRetinaUrl: "/assets/icons/faskes.png",
+    popupAnchor: [-0, -0],
+    iconSize: [30, 30],
+});
+
+export default faskesIcon;
