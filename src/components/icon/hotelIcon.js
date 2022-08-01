3eddfd8d7cd2133a2fd74cecf7bf1491d6d6d568
anwark7 <anwarkhairuddin11@gmail.com>
2022-08-01 12:35:43 +0700

    feat: add hotel marker

---
+++ src/components/icon/hotelIcon.js
@@ -0,0 +1,10 @@
+import L from "leaflet";
+
+const hotelIcon = new L.Icon({
+    iconUrl: "/assets/icons/hotel.png",
+    iconRetinaUrl: "/assets/icons/hotel.png",
+    popupAnchor: [-0, -0],
+    iconSize: [30, 30],
+});
+
+export default hotelIcon;
