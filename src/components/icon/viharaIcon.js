6c6eaf68a4895fafce7db2285422bfb9291d6b36
anwark7 <anwarkhairuddin11@gmail.com>
2022-08-01 12:36:25 +0700

    feat: add vihara marker

---
+++ src/components/icon/viharaIcon.js
@@ -0,0 +1,10 @@
+import L from "leaflet";
+
+const viharaIcon = new L.Icon({
+    iconUrl: "/assets/icons/vihara.png",
+    iconRetinaUrl: "/assets/icons/vihara.png",
+    popupAnchor: [-0, -0],
+    iconSize: [30, 30],
+});
+
+export default viharaIcon;
