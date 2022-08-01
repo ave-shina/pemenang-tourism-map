2bb7f217adc13cfed25f94f50fbafd49f021174a
anwark7 <anwarkhairuddin11@gmail.com>
2022-08-01 12:35:27 +0700

    feat: add pelabuhan marker

---
+++ src/components/icon/pelabuhanIcon.js
@@ -0,0 +1,10 @@
+import L from "leaflet";
+
+const pelabuhanIcon = new L.Icon({
+    iconUrl: "/assets/icons/anchor.png",
+    iconRetinaUrl: "/assets/icons/anchor.png",
+    popupAnchor: [-0, -0],
+    iconSize: [30, 30],
+});
+
+export default pelabuhanIcon;
