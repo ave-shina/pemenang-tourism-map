e9608bbf9e408fb55b1ada6cdb4b4507b933f3c2
anwark7 <anwarkhairuddin11@gmail.com>
2022-08-01 12:36:37 +0700

    feat: add wisata lain marker

---
+++ src/components/icon/wisatalainIcon.js
@@ -0,0 +1,10 @@
+import L from "leaflet";
+
+const wisatalainIcon = new L.Icon({
+    iconUrl: "/assets/icons/wisatalain.png",
+    iconRetinaUrl: "/assets/icons/wisatalain.png",
+    popupAnchor: [-0, -0],
+    iconSize: [30, 30],
+});
+
+export default wisatalainIcon;
