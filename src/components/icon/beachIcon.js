9681d4af5589cd88eb56d54a8faa225d40d2d1a8
anwark7 <anwarkhairuddin11@gmail.com>
2022-08-01 12:33:57 +0700

    feat: add beach marker

---
+++ src/components/icon/beachIcon.js
@@ -0,0 +1,10 @@
+import L from "leaflet";
+
+const beachIcon = new L.Icon({
+    iconUrl: "/assets/icons/beach.png",
+    iconRetinaUrl: "/assets/icons/beach.png",
+    popupAnchor: [-0, -0],
+    iconSize: [30, 30],
+});
+
+export default beachIcon;
