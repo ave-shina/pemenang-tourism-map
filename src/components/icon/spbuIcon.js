2ee9011c08474db1905099ed93b434385802ea72
anwark7 <anwarkhairuddin11@gmail.com>
2022-08-01 12:36:12 +0700

    feat: add spbu marker

---
+++ src/components/icon/spbuIcon.js
@@ -0,0 +1,10 @@
+import L from "leaflet";
+
+const spbuIcon = new L.Icon({
+    iconUrl: "/assets/icons/spbu.png",
+    iconRetinaUrl: "/assets/icons/spbu.png",
+    popupAnchor: [-0, -0],
+    iconSize: [30, 30],
+});
+
+export default spbuIcon;
