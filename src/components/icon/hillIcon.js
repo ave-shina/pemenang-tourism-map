20c34b888626c01ed68025992abd374e0f41afb9
anwark7 <anwarkhairuddin11@gmail.com>
2022-08-01 12:34:58 +0700

    feat: add hill marker

---
+++ src/components/icon/hillIcon.js
@@ -0,0 +1,10 @@
+import L from "leaflet";
+
+const hillIcon = new L.Icon({
+    iconUrl: "/assets/icons/hill.png",
+    iconRetinaUrl: "/assets/icons/hill.png",
+    popupAnchor: [-0, -0],
+    iconSize: [30, 30],
+});
+
+export default hillIcon;
