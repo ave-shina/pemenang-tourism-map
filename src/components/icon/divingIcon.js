705f994651c3fdf6b389a6deb7aea8c2e1645a33
anwark7 <anwarkhairuddin11@gmail.com>
2022-08-01 12:34:10 +0700

    feat: add diving marker

---
+++ src/components/icon/divingIcon.js
@@ -0,0 +1,10 @@
+import L from "leaflet";
+
+const divingIcon = new L.Icon({
+    iconUrl: "/assets/icons/diving.png",
+    iconRetinaUrl: "/assets/icons/diving.png",
+    popupAnchor: [-0, -0],
+    iconSize: [30, 30],
+});
+
+export default divingIcon;
