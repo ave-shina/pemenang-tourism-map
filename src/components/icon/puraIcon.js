b99b2970721727135472b651da3180cd3c245937
anwark7 <anwarkhairuddin11@gmail.com>
2022-08-01 12:35:58 +0700

    feat: add pura marker

---
+++ src/components/icon/puraIcon.js
@@ -0,0 +1,10 @@
+import L from "leaflet";
+
+const puraIcon = new L.Icon({
+    iconUrl: "/assets/icons/pura.png",
+    iconRetinaUrl: "/assets/icons/pura.png",
+    popupAnchor: [-0, -0],
+    iconSize: [30, 30],
+});
+
+export default puraIcon;
