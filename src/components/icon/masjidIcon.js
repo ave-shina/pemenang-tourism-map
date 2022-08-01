2bd9ebcabd07192623881ce9733505b0b16dcd8f
anwark7 <anwarkhairuddin11@gmail.com>
2022-08-01 12:35:10 +0700

    feat: add masjid marker

---
+++ src/components/icon/masjidIcon.js
@@ -0,0 +1,10 @@
+import L from "leaflet";
+
+const masjidIcon = new L.Icon({
+    iconUrl: "/assets/icons/mosque.png",
+    iconRetinaUrl: "/assets/icons/mosque.png",
+    popupAnchor: [-0, -0],
+    iconSize: [30, 30],
+});
+
+export default masjidIcon;
