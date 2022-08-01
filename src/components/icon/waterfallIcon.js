67d5e49c4f18dedec9c2db34fca37fe94be44283
anwark7 <anwarkhairuddin11@gmail.com>
2022-08-01 12:36:50 +0700

    feat: waterfall marker

---
+++ src/components/icon/waterfallIcon.js
@@ -0,0 +1,10 @@
+import L from "leaflet";
+
+const waterfallIcon = new L.Icon({
+    iconUrl: "/assets/icons/waterfall.png",
+    iconRetinaUrl: "/assets/icons/waterfall.png",
+    popupAnchor: [-0, -0],
+    iconSize: [30, 30],
+});
+
+export default waterfallIcon;
