da1050acfd60abcef92a5e9e0be4d2f30c1ffb38
anwark7 <anwarkhairuddin11@gmail.com>
2022-08-01 12:34:27 +0700

    feat: add ekowisata marker

---
+++ src/components/icon/ekowisataIcon.js
@@ -0,0 +1,10 @@
+import L from "leaflet";
+
+const ekowisataIcon = new L.Icon({
+    iconUrl: "/assets/icons/jungle.png",
+    iconRetinaUrl: "/assets/icons/jungle.png",
+    popupAnchor: [-0, -0],
+    iconSize: [30, 30],
+});
+
+export default ekowisataIcon;
